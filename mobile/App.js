3cf851b19f3d52a92fe21c57d1f50492d2d275ab
Diego Paz <diegofernandes14@hotmail.com>
2020-01-27 18:02:09 -0300

    FIX

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+
+import Routes from './src/routes'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Diego Fernandes</Text>
-    </View>
+    <>
+    <StatusBar  barStyle="light-content" backgroundColor = "#7D40E7 " />
+    <Routes />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
