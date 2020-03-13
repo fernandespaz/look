54ffc43c2ac090113cc2930a4b7419e2a88afeab
C1313769 Diego Fernandes Paz <c1313769@interno.bb.com.br>
2020-03-13 13:08:28 -0300

    Beckend

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,28 @@
 const { Router } = require('express');
-const  DevController = require ('./controllers/DevController');
-const SeachController = require ('./controllers/SeachController');
+const axios = require('axios');
+const Dev = require('./models/dev');
 
 const routes = Router();
-//get, post, put, delete
-routes.get('/devs', DevController.index);
-routes.post('/devs', DevController.store);
 
-routes.get('/search', SeachController.Index);
+//get, post, put, delete
+
+routes.post('/devs', async (req,res)=>{
+    const {github_username,techs} = req.body;
+    
+    const response = await axios.get(`http://api.github.com/users/${github_username}`)
+    
+    const { name = login, avatar_url, bio} = response.data;
+    const techsArray = techs.split(',').map(techs => techs.trim());
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+    })
+
+    return res.json(dev);
+});
+
 module.exports = routes;
